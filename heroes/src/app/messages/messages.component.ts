d1762416a0c26822cbc26fd192e5c12fb009c6df
Egor Rudkov <egor.rudkov@t-systems.com>
2023-09-05 13:23:15 +0200

    Add services

---
+++ heroes/src/app/messages/messages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent {
+  // messageService MUST be public because of binds in the template
+  constructor(public messageService: MessageService) {}
+}
