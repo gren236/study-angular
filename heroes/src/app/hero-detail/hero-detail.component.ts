e52038818c086dc41ce2fc4d428eec686a5f2ae1
Egor Rudkov <egor.rudkov@t-systems.com>
2023-09-05 14:59:08 +0200

    Add HTTP client and requests

--- heroes/src/app/hero-detail/hero-detail.component.ts
+++ heroes/src/app/hero-detail/hero-detail.component.ts
@@ -27,4 +27,10 @@ export class HeroDetailComponent implements OnInit {
   goBack() {
     this.location.back();
   }
+
+  save() {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+    }
+  }
 }
